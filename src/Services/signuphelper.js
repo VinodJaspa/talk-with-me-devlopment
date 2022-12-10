c68a7651f133372553701c957f57782bfa4a827b
Vinod-sfs <sfs.vinod22@gmail.com>
2022-12-11 01:17:18 +0530

    domain added

--- src/Services/signuphelper.js
+++ src/Services/signuphelper.js
@@ -75,7 +75,7 @@ export const createUser = async (values) => {
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000',
+    url: 'https://fantastic-sherbet-f9a08c.netlify.app',
     // This must be true.
     handleCodeInApp: true,
     iOS: {
