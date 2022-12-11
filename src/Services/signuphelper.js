e0a05ae43b4f1b297400c2fc95593c1d5a69d401
Vinod-sfs <sfs.vinod22@gmail.com>
2022-12-11 21:25:10 +0530

    turutty

--- src/Services/signuphelper.js
+++ src/Services/signuphelper.js
@@ -1,7 +1,7 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { collection, doc, onSnapshot, setDoc } from "firebase/firestore";
-import { db } from "../Firebase/firebaseconfig";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
+import { db } from "../Firebase/firebaseconfig";
 
 export const createUser = async (values) => {
     const { email, password, username } = values;
@@ -18,38 +18,33 @@ export const createUser = async (values) => {
             //create doc file in firestore
             //Creatse a new collection and save user info
 
-            res.user.sendEmailVerification().then(() => {
-                // Email verification sent!
-                auth.signOut();
-                toast.success("Email Verification sent! Check your mail box", { theme: "colored" });
+            sendEmailVerification(auth.currentUser)
+                .then(() => {
+                    // Email verification sent!
+                    toast.success("Email Verification sent! Check your mail box", { theme: "colored" });
+                    setDoc(doc(db, 'users', createdAt), {
+                        ...res.user.metadata,
+                        email: email,
+                        username: username,
+                        photoURL: photoURL,
+                        uid: uid
+                    })
+                        .then(() => {
+                            console.log('Document successfully written!')
+                            toast.success("You have successfully created your account", { theme: "colored" });
+                            isSuccess = true;
+                        })
+                        .catch((error) => {
+                            toast.error("Oops! Something went wrong!", { theme: "colored" });
+                            console.error('Error writing document: ', error)
+                            return isSuccess;
+                        })
 
-                setDoc(doc(db, 'users', createdAt), {
-                    ...res.user.metadata,
-                    email: email,
-                    username: username,
-                    photoURL: photoURL,
-                    uid: uid
+                  }).catch((error) => {
+                    toast.error("Oops! Something went wrong!", { theme: "colored" });
+                    console.error('Error writing document: ', error)
+                    return isSuccess;
                 })
-                    .then(() => {
-                        console.log('Document successfully written!')
-                        toast.success("You have successfully created your account", { theme: "colored" });
-                        isSuccess = true;
-
-
-
-                    })
-                    .catch((error) => {
-                        toast.error("Oops! Something went wrong!", { theme: "colored" });
-
-                        console.error('Error writing document: ', error)
-                        return isSuccess;
-                    })
-            }).catch((error) => {
-                toast.error("Oops! Something went wrong!", { theme: "colored" });
-
-                console.error('Error writing document: ', error)
-                return isSuccess;
-            })
 
         }
     }
